e81486d22f5fb34f3d838d0bcfd06b7db858c7bf
EC2 Default User <ec2-user@ip-172-31-29-43.us-east-2.compute.internal>
2021-09-30 23:37:51 +0000

    Figured Out Stories for Filter Movie List

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -15,10 +16,17 @@ end
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
+
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  idx1 = page.body.index(e1)
+  idx2 = page.body.index(e2)
+  
+  order = idx1 < idx2
+  
+  order.should be true
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
